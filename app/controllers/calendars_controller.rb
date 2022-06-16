7025f8d73bd14d1ccd75014cfd61009ab8ec58e8
toyolife881 <toyolife.island881@gmail.com>
2022-06-16 17:01:17 +0900

    Issue2の再修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,6 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    # getWeek
     get_week
     @plan = Plan.new
   end
@@ -19,7 +18,6 @@ class CalendarsController < ApplicationController
     params.require(:calendars).permit(:date, :plan)
   end
 
-  # def getWeek
   def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
